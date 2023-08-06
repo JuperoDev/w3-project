ad3cd4c256c05fa3b841b2406cf9e11ed5bdeb46
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-06 14:23:54 +0200

    rendering army

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="app-container">
-
+  <div class="app">
+    <div class="app-army-container">
+      hola
+      <ArmyFetch />
+    </div>
   </div>
 </template>
+
+<script>
+import ArmyFetch from '@/components/armyComponents/ArmyFetch.vue';
+
+export default {
+  components: {
+    ArmyFetch
+  }
+}
+</script>
---
+++ components/armyComponents/ArmyFetch.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <div>
+    <v-btn>
+      wololo
+    </v-btn>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const data = ref(null);
+
+onMounted(async () => {
+  try {
+    const res = await fetch("/army.json");
+    data.value = await res.json();
+    console.log("Fetched Data: ", data.value);
+  } catch (error) {
+    console.error("Fetch Error: ", error);
+  }
+});
+</script>
+<style lang=""></style>

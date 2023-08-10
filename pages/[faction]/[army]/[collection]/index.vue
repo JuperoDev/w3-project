ff150423406a5916b125bb03ee9547ff57d10ddd
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-11 01:58:01 +0200

    Update index.vue

--- pages/[faction]/[army]/[collection]/index.vue
+++ pages/[faction]/[army]/[collection]/index.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>
-      parent unit
+  <div>
+    <h1>{{ unitName }}</h1>
+    <p>{{ lore }}</p>
+  </div>
+</template>
 
-    </div>
-  </template>
-  
-  <script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
-  </script>
-  
+const route = useRoute();
+const unitName = route.params.armyName;
+const faction = route.params.faction;
+const army = route.params.army;
+
+const lore = ref("");
+
+onMounted(async () => {
+  try {
+    const res = await fetch(`/faction/${faction}/${army}/collection/${unitName}.json`);
+    const unitData = await res.json();
+    lore.value = unitData.lore;
+  } catch (error) {
+    console.error("Fetch Error: ", error);
+  }
+});
+</script>

9a5d6486a1002b54ebb09292c92631eb4f893ffc
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-15 23:51:17 +0200

    Update index.vue

--- pages/[faction]/[army]/[collection]/index.vue
+++ pages/[faction]/[army]/[collection]/index.vue
@@ -4,12 +4,7 @@
 
     <Attributes :attributes="attributes" />
 
-   
-
-    
- 
     <v-expansion-panels>
-
       <!-- Ranged weapons -->
       <v-expansion-panel>
         <v-expansion-panel-title>
@@ -33,10 +28,48 @@
           </div>
         </v-expansion-panel-text>
       </v-expansion-panel>
+
+      <!-- Abilities  -->
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <div class="uppercase font-semibold">Abilities</div>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+
+      <!-- Wargear Options  -->
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <div class="uppercase font-semibold">wargear options</div>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+      <!-- unit composition  -->
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <div class="uppercase font-semibold">unit composition</div>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+      <!-- keywords  -->
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <div class="uppercase font-semibold">keywords</div>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+      <!-- end panels  -->
     </v-expansion-panels>
 
     <Lore :lore="lore" />
-
   </div>
 </template>
 

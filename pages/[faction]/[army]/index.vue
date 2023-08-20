a076af25223150bbbb296e62eb098252d09935f0
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-20 12:00:28 +0200

    sanitize code

--- pages/[faction]/[army]/index.vue
+++ pages/[faction]/[army]/index.vue
@@ -3,7 +3,7 @@
     <h1>{{ faction }} - {{ army }}</h1>
     <div v-if="armyData">
       <div v-for="armyName in armyData.units" :key="armyName" class="m-5">
-        <nuxt-link :to="`/${faction}/${army}/${armyName}`">
+        <nuxt-link :to="generateLink(faction, army, armyName)">
           <v-btn>
             <p>{{ armyName }}</p>
           </v-btn>
@@ -25,11 +25,17 @@ const replaceUnwantedCharacters = (input) => {
   return input.replace(/[^a-zA-Z0-9-]/g, "-");
 };
 
-const sanitizedFaction = replaceUnwantedCharacters(faction);
-const sanitizedArmy = replaceUnwantedCharacters(army);
+// Function to generate sanitized link
+const generateLink = (faction, army, armyName) => {
+  const sanitizedArmyName = replaceUnwantedCharacters(armyName);
+  return `/${faction}/${army}/${sanitizedArmyName}`;
+};
 
 onMounted(async () => {
   try {
+    const sanitizedFaction = replaceUnwantedCharacters(faction);
+    const sanitizedArmy = replaceUnwantedCharacters(army);
+
     const res = await fetch(`/faction/${sanitizedFaction}/${sanitizedArmy}/collection.json`);
     armyData.value = await res.json();
     console.log("Fetching URL:", `/faction/${sanitizedFaction}/${sanitizedArmy}/collection.json`); // Log the URL being fetched

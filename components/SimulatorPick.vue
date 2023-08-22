24ebbef03a4c2bdeb0fecd2ba846b58aa2dab3e6
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-23 01:31:05 +0200

    Update SimulatorPick.vue

--- components/SimulatorPick.vue
+++ components/SimulatorPick.vue
@@ -1,16 +1,41 @@
-<template lang="">
-    <div>
+<template>
+    <div >
         <v-select
-  label="Select"
-  :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-></v-select>
+            label="Select Faction"
+            :items="factions"
+            
+        ></v-select>
     </div>
 </template>
+
 <script>
+import { ref, onMounted } from 'vue';
+
 export default {
-    
+    setup() {
+        const factions = ref([]);
+
+        // Fetch the data from the JSON file
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/faction.json');
+                const jsonData = await response.json();
+                factions.value = jsonData.faction;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        // Fetch the data when the component is mounted
+        onMounted(fetchData);
+
+        return {
+            factions
+        };
+    }
 }
 </script>
-<style lang="">
+
+<style>
     
 </style>

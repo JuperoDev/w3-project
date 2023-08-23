afb2dfc5d6b3e59536749addb8f436c3c32c8809
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-23 14:51:08 +0200

    simulator selectors

--- components/SimulatorPick.vue
+++ components/SimulatorPick.vue
@@ -1,41 +1,61 @@
 <template>
-    <div >
-        <v-select
-            label="Select Faction"
-            :items="factions"
-            
-        ></v-select>
+    <div>
+      <v-select label="Select Faction" :items="factions" v-model="selectedFaction"></v-select>
+  
+      <v-select label="Select Army" :items="selectedFactionData?.army || []" v-model="selectedArmy" :disabled="!selectedFaction"></v-select>
+  
+      <v-select label="Select Unit" :items="selectedArmyData?.units || []" v-model="selectedCollection" :disabled="!selectedArmy"></v-select>
     </div>
-</template>
-
-<script>
-import { ref, onMounted } from 'vue';
-
-export default {
-    setup() {
-        const factions = ref([]);
-
-        // Fetch the data from the JSON file
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/faction.json');
-                const jsonData = await response.json();
-                factions.value = jsonData.faction;
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        // Fetch the data when the component is mounted
-        onMounted(fetchData);
-
-        return {
-            factions
-        };
+  </template>
+  
+  <script setup>
+  import { ref, onMounted, watch } from 'vue';
+  
+  const factions = ref([]);
+  const selectedFaction = ref('');
+  const selectedFactionData = ref(null);
+  const selectedArmy = ref('');
+  const selectedArmyData = ref(null);
+  const selectedCollection = ref('');
+  
+  // Fetch the data from the JSON file based on the selected faction
+  const fetchFactionData = async () => {
+    try {
+      const response = await fetch(`/faction/${selectedFaction.value}/army.json`);
+      selectedFactionData.value = await response.json();
+      selectedArmy.value = ''; // Reset selected army when faction changes
+    } catch (error) {
+      console.error('Error fetching faction data:', error);
     }
-}
-</script>
-
-<style>
-    
-</style>
+  };
+  
+  // Fetch the data from the JSON file based on the selected army
+  const fetchArmyData = async () => {
+  try {
+    const response = await fetch(`/faction/${selectedFaction.value}/${selectedArmy.value}/collection.json`);
+    selectedArmyData.value = await response.json();
+    selectedCollection.value = ''; // Reset selected collection when army changes
+    console.log('Fetched Army Data:', selectedArmyData.value);
+  } catch (error) {
+    console.error('Error fetching army data:', error);
+  }
+};
+  
+  // Fetch the faction list data when the component is mounted
+  onMounted(async () => {
+    try {
+      const response = await fetch('/faction.json');
+      const jsonData = await response.json();
+      factions.value = jsonData.faction;
+    } catch (error) {
+      console.error('Error fetching faction list:', error);
+    }
+  });
+  
+  // Watch for changes in the selected faction and fetch corresponding data
+  watch(selectedFaction, fetchFactionData);
+  
+  // Watch for changes in the selected army and fetch corresponding data
+  watch(selectedArmy, fetchArmyData);
+  </script>
+  

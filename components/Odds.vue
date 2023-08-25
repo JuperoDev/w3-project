115e3d342cf74e07c0cf2b72ba9349f76c69fb94
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-25 22:38:25 +0200

    Update Odds.vue

--- components/Odds.vue
+++ components/Odds.vue
@@ -1,28 +1,11 @@
-<template>
+<template >
     <div>
-    Simulator lab
-    <br>
-   strong:  {{ strong }} 
-   <br>
-tough: {{ tough }}
-<br>
-{{ strongerThanTough }}
+        
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, computed } from 'vue';
- 
-  const strong = ref (4)
-  const tough = ref (4)
+</template>
+<script setup>
 
-  const strongerThanTough = computed(() => {
-  return strong.value > tough.value;
-});
-  
-  </script>
-  
-  <style>
-  /* Your CSS styling here */
-  </style>
-  
+</script>
+<style>
+    
+</style>

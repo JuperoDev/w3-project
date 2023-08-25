332d712fa9709d7572a76c5a044ad6380f2f01f9
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-25 22:37:49 +0200

    attempt to recover

---
+++ components/Odds.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    Simulator lab
+    <br>
+   strong:  {{ strong }} 
+   <br>
+tough: {{ tough }}
+<br>
+{{ strongerThanTough }}
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, computed } from 'vue';
+ 
+  const strong = ref (4)
+  const tough = ref (4)
+
+  const strongerThanTough = computed(() => {
+  return strong.value > tough.value;
+});
+  
+  </script>
+  
+  <style>
+  /* Your CSS styling here */
+  </style>
+  

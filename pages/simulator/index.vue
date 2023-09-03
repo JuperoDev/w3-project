3f0a7b472472cf111f28b3db61362f0a5f57f755
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-04 00:34:53 +0200

    Update index.vue

--- pages/simulator/index.vue
+++ pages/simulator/index.vue
@@ -1,5 +1,8 @@
 <template>
+    <div class="simulator-container grid grid-cols-2 gap-4">
     <div>
         <SimulatorPick :attack-role="true"/>
+        <SimulatorPick :attack-role="false"/>
     </div>
+</div>
 </template>

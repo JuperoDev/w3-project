2dbf840092215f3f41eca5fa6da9992935c406bb
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-22 00:40:44 +0200

    Update Datasheet.vue

--- components/desktop/Datasheet.vue
+++ components/desktop/Datasheet.vue
@@ -5,16 +5,39 @@
       
     </div>
     <div class="datasheet-desktop--grid-container grid grid-cols-3">
+
       <div class="datasheet-desktop__grid-container--left col-span-2">
         
-        <p>Ranged Weapons</p>
-        <p>Melee Weapons</p>
-        <p>Wargear Options</p>
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3 grid grid-cols-2">
+          <div class="datasheet-desktop__section-title">
+            <p>Ranged Weapons</p>
+          </div>
+
+          <div class="datasheet-desktop__section-stats">
+            <p>Range</p>
+          </div>
+
+        </div>
+
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Melee weapons</p>
+        </div>
+
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Wargear Options</p>
+        </div>
+       
       </div>
       <div class="datasheet-desktop__grid-container--right">
-        <p>Abilities</p>
-        <p>Unit Compoition</p>
-        <p>Leader</p>
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Abilities</p>
+        </div>
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Unit Composition</p>
+        </div>
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Leader</p>
+        </div>
       </div>
     </div>
   </div>

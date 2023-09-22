fbb22f2d23ced4865e2895fe22c95cd98bfca0a4
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-23 01:29:12 +0200

    Update Keywords.vue

--- components/desktop/components/Keywords.vue
+++ components/desktop/components/Keywords.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        Hola
+ 
+        <div
+      class="datasheet-desktop--grid-container grid grid-cols-3 border-t-0 border-r-2 border-b-2 border-l-2 border-solid border-slate-700 mx-2"
+    >
+      <div class="datasheet-desktop__grid-container--left col-span-2">
+        KEYWORDS: INFANTRY, CHARACTER, EPIC HERO, GREAT DEVOURER, DEATHLEAPER
+      </div>
+
+    
+      <div
+        class="datasheet-desktop__grid-container--right  border-slate-700 grid grid-cols-2"
+      >
+      <div class="diamond bg-slate-700 ">
+        
     </div>
+    <div class="texto">      FACTION KEYWORDS:
+TYRANIDS</div>
+
+      </div>
+    </div>
+   
 </template>
+
+<style scoped>
+.diamond{
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    height: 80px;
+    width: 80px;
+    margin-left: -39px ;
+}
+</style>

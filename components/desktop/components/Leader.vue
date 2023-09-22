ebe269f9e2aff13bf444460322cf02796dfa0f9e
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-22 17:48:23 +0200

    update

--- components/desktop/components/Leader.vue
+++ components/desktop/components/Leader.vue
@@ -1,11 +1,21 @@
 <template>
-    <div
-          class="datasheet-desktop__section-header bg-slate-400 m-3"
-        >
-        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
-          <p>Leader</p>
-        </div>
+  <div class="datasheet-desktop__leader-container">
+    <div class="datasheet-desktop__leader--header bg-slate-400 mx-3 p-1">
+      <p>Leader</p>
+    </div>
 
- 
-        </div>
+<div class="datasheet-desktop__leader--body my-3 pl-5 ">
+    <p>This model can be attached to the following unit:</p>
+
+
+    <ul class="my-2 pl-5">
+      <li class="capitalize">gargoyles</li>
+
+      <li class="capitalize">tyranid warriors with melee bio-weapons</li>
+
+      <li class="capitalize">tyranid warriors with ranged bio-weapons</li>
+    </ul>
+
+</div>
+  </div>
 </template>

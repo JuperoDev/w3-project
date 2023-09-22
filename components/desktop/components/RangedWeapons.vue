bf66b7773840e007b5c1e317c36331ff61b2baa7
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-23 00:39:19 +0200

    Update RangedWeapons.vue

--- components/desktop/components/RangedWeapons.vue
+++ components/desktop/components/RangedWeapons.vue
@@ -1,21 +1,63 @@
 <template>
+  <div class="ranged-weapons__container">
     <div
-          class="datasheet-desktop__section-header bg-slate-400 m-3 grid grid-cols-2"
-        >
-          <div class="datasheet-desktop__section-title">
-            <p>Ranged Weapons</p>
-          </div>
+      class="datasheet-desktop__section-header bg-slate-400 m-3 grid grid-cols-2"
+    >
+      <div class="datasheet-desktop__section-title">
+        <p>Ranged Weapons</p>
+      </div>
 
-          <div
-            class="datasheet-desktop__section-stats grid grid-cols-6 text-center"
-          >
-            <p>Range</p>
+      <div
+        class="datasheet-desktop__section-stats grid grid-cols-6 text-center"
+      >
+        <p>Range</p>
 
-            <p>A</p>
-            <p>BS</p>
-            <p>S</p>
-            <p>AP</p>
-            <p>D</p>
-          </div>
-        </div>
+        <p>A</p>
+        <p>BS</p>
+        <p>S</p>
+        <p>AP</p>
+        <p>D</p>
+      </div>
+    </div>
+
+    <div
+      class="ranged-weapons__weapon   border-b-2  border-dotted border-slate-400 m-3 grid grid-cols-2"
+    >
+      <div class="datasheet-desktop__section-title">
+        <p>Fires of the Abyss</p>
+      </div>
+
+      <div
+        class="datasheet-desktop__section-stats grid grid-cols-6 text-center"
+      >
+        <p>12"</p>
+
+        <p>2D6</p>
+        <p>2+</p>
+        <p>5</p>
+        <p>-1</p>
+        <p>1</p>
+      </div>
+    </div>
+
+    <div
+      class="ranged-weapons__weapon m-3 grid grid-cols-2  border-b-2  border-dotted border-slate-400"
+    >
+      <div class="datasheet-desktop__section-title">
+        <p>Inferno bolt pistol</p>
+      </div>
+
+      <div
+        class="datasheet-desktop__section-stats grid grid-cols-6 text-center"
+      >
+        <p>12"</p>
+        <p>1</p>
+        <p>2+</p>
+        <p>4</p>
+        <p>-1</p>
+        <p>1</p>
+      </div>
+    </div>
+
+  </div>
 </template>

694f6649b87143475ae4f7a5144c2c90e35b7ab1
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-22 11:52:13 +0200

    Update Attributes.vue

--- components/desktop/components/Attributes.vue
+++ components/desktop/components/Attributes.vue
@@ -1,16 +1,99 @@
 <template>
   <div>
-    <!-- caja -->
-    <div
-      class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
-    >
-      <div
-        class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
-      >
-        20 "
+    <!-- boxes -->
+    <div class="datasheet__header--stats grid grid-cols-6 place-items-center">
+      <!-- Single Box -->
+      <div class="datasheet__header--stats-single grid place-items-center">
+        M
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            8 "
+          </div>
+        </div>
       </div>
+      <!-- end Single Box  -->
+
+            <!-- Single Box -->
+            <div class="datasheet__header--stats-single grid place-items-center">
+        T
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            4
+          </div>
+        </div>
+      </div>
+      <!-- end Single Box  -->
+
+            <!-- Single Box -->
+            <div class="datasheet__header--stats-single grid place-items-center">
+        SV
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            3+
+          </div>
+        </div>
+      </div>
+      <!-- end Single Box  -->
+
+            <!-- Single Box -->
+            <div class="datasheet__header--stats-single grid place-items-center">
+        W
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            6
+          </div>
+        </div>
+      </div>
+      <!-- end Single Box  -->
+
+            <!-- Single Box -->
+            <div class="datasheet__header--stats-single grid place-items-center">
+        LD
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            6 +
+          </div>
+        </div>
+      </div>
+      <!-- end Single Box  -->
+
+            <!-- Single Box -->
+            <div class="datasheet__header--stats-single grid place-items-center">
+        OC
+        <div
+          class="datasheet__header--stats-single-box p-1 w-11 h-11 bg-zinc-500 text-zinc-900"
+        >
+          <div
+            class="datasheet__header--stats-single-box w-9 h-9 bg-slate-200 grid place-items-center"
+          >
+            1
+          </div>
+        </div>
+      </div>
+      <!-- end Single Box  -->
     </div>
-    <!-- end caja  -->
+
+    <!-- end boxes  -->
   </div>
 </template>
 

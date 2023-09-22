9c4785e8182ecb0883718d205f00b872faef8326
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-22 17:16:25 +0200

    Create Abilities.vue

---
+++ components/desktop/components/Abilities.vue
@@ -0,0 +1,11 @@
+<template>
+    <div
+          class="datasheet-desktop__section-header bg-slate-400 m-3"
+        >
+        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+          <p>Abilities</p>
+        </div>
+
+ 
+        </div>
+</template>

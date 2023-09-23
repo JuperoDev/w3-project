272e065aa97be2756dde9d0ff0c15d3783f943c6
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-23 20:49:25 +0200

    Update Keywords.vue

--- components/desktop/components/Keywords.vue
+++ components/desktop/components/Keywords.vue
@@ -1,38 +1,31 @@
 <template>
- 
-        <div
-      class="datasheet-desktop--grid-container grid grid-cols-12  mx-2  -mt-4 border-r-2 border-b-2 border-l-2  border-solid border-slate-700  "
-    >
-
+  <div
+    class="datasheet-desktop--grid-container grid grid-cols-12 mx-2 -mt-4 border-b-2 border-l-2 border-r-2 border-solid border-slate-700"
+  >
+    <div class="emptycolumn col-span-1"></div>
     <!-- border-t-0 border-r-2 border-b-2 border-l-2 border-solid border-slate-700 -->
-      <div class="datasheet-desktop__grid-container--left  p-8 col-span-8 ">
-        KEYWORDS: INFANTRY, CHARACTER, EPIC HERO, GREAT DEVOURER, DEATHLEAPER
-      </div>
-
-    
-      
-      <div class="diamond bg-slate-700 col-span-1 ">
-        
+    <div class="datasheet-desktop__grid-container--left p-8 col-span-7 ">
+      KEYWORDS: INFANTRY, CHARACTER, EPIC HERO, GREAT DEVOURER, DEATHLEAPER
     </div>
-    <div class="texto col-span-3 p-8 -ml-20   ">      FACTION KEYWORDS:
-TYRANIDS</div>
 
-      </div>
+    <div class="diamond bg-slate-700 col-span-1"></div>
+    <div class="texto col-span-3 p-8 -ml-20">FACTION KEYWORDS: TYRANIDS</div>
     
-   
+  </div>
 </template>
 
 <style scoped>
-.diamond{
-    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
-    height: 80px;
-    width: 80px;
-    margin-left: -39px ;
+.diamond {
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+  height: 80px;
+  width: 80px;
+  margin-left: -39px;
 }
 
-.datasheet-desktop--grid-container{
+.datasheet-desktop--grid-container {
   height: 68px;
-  
 }
 
+div {
+}
 </style>

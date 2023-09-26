1ccae01f1100a2eb80bd277cd228995500588841
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-27 01:14:14 +0200

    Update Abilities.vue

--- components/desktop/components/Abilities.vue
+++ components/desktop/components/Abilities.vue
@@ -6,18 +6,25 @@
           <p>Abilities</p>
         </div>
         <div class="abilities-container p-3">
+          <div class="faction-container" v-if="abilities.core && abilities.core.length > 0"  >
         <span class="font-semibold">Core: </span>
         
          <span v-for="ability in abilities.core" class="capitalize">{{ ability }}, </span> 
-      <br/>
+          </div>
+      
+
+      <div class="faction-container" v-if="abilities.faction && abilities.faction.length > 0" >
       <span class="font-semibold">Faction: </span>
       <span class="capitalize" v-for="ability in abilities.faction" >
           {{ ability }}, 
       </span>
+    </div>
+
+    <div class="faction-container" v-if="abilities.otherAbilities && abilities.otherAbilities.length > 0" >
       <div v-for="(otherAbility, index) in abilities.otherAbilities" :key="'misc-' + index">
           <span class="font-semibold">{{ otherAbility.name }} : </span> <span>{{ otherAbility.description }}</span>
       </div>
- 
+    </div>
         </div>
       </div>
 </template>

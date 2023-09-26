2624dda8a97709ecada41834b841c44991f79f72
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-09-27 01:16:21 +0200

    Update Abilities.vue

--- components/mobile/Abilities.vue
+++ components/mobile/Abilities.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="abilities-container p-3">
+    <div class="singe-ability-check" v-if="abilities.core && abilities.core.length > 0">
         <span class="font-semibold">Core: </span>
         
          <span v-for="ability in abilities.core" class="capitalize">{{ ability }}, </span> 
-      <br/>
+        </div>
+      <div class="singe-ability-check" v-if="abilities.faction && abilities.faction.length > 0">
       <span class="font-semibold">Faction: </span>
       <span class="capitalize" v-for="ability in abilities.faction" >
           {{ ability }}, 
       </span>
+    </div>
+      <div class="singe-ability-check" v-if="abilities.otherAbilities && abilities.core.otherAbilities > 0">
       <div v-for="(otherAbility, index) in abilities.otherAbilities" :key="'misc-' + index">
           <span class="font-semibold">{{ otherAbility.name }} : </span> <span>{{ otherAbility.description }}</span>
       </div>
- 
+    </div>
         </div>
 </template>
 
 <script setup>
 
 const props = defineProps({
-    abilities: Object,Boolean
+    abilities: Object
 });
 </script>

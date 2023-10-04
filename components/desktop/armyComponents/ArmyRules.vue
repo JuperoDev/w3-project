3b719d40fc71f112c5c4c7e4d715d2f2f5188bb7
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-04 04:58:07 +0200

    update

--- components/desktop/armyComponents/ArmyRules.vue
+++ components/desktop/armyComponents/ArmyRules.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="army-rules-container">
-    <h2>{{army}} Army Rules</h2>
+    <h2 class="text-3xl uppercase px-10">{{ army }} Army Rules</h2>
     <div class="army-rules__grid grid grid-cols-2 gap-4">
-
-<div class="army-rules__grid-element" v-for="rule in armyRules">
-    <h3>{{ rule.name }}</h3>
-    <p>{{ rule.description }}</p>
-
-</div>
-
-
+      <div class="army-rules__grid-element p-10" v-for="rule in armyRules">
+        <h4 class="text-2xl uppercase">{{ rule.name }}</h4>
+        <p class="mt-5">{{ rule.description }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +13,9 @@
 <script setup>
 import { useRoute } from "vue-router";
 
- const props = defineProps({
+const props = defineProps({
   armyRules: Array,
- });
+});
 
 // params
 const route = useRoute();
--- components/desktop/armyComponents/Introduction.vue
+++ components/desktop/armyComponents/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="introduction-container">
-        <h1>introduction</h1>
-        <p>On this page you will find several {{army}} Detachments you can select from, as described in the Core Rules (see the Select Detachment Rules step). If you are playing a Crusade battle, see the equivalent step in the Mustering a Crusade Army section of the main Crusade rules. Each Detachment is a themed fighting force that focuses on one particular combat style employed by the Tyranids - the rules they include are as follows:</p>
+    <div class="introduction-container p-10">
+        <h3 class="text-3xl uppercase ">introduction</h3>
+        <p class="mt-5">On this page you will find several {{army}} Detachments you can select from, as described in the Core Rules (see the Select Detachment Rules step). If you are playing a Crusade battle, see the equivalent step in the Mustering a Crusade Army section of the main Crusade rules. Each Detachment is a themed fighting force that focuses on one particular combat style employed by the Tyranids - the rules they include are as follows:</p>
     </div>
 </template>
 

8cfda8f9cc7724bbbfa55db84a071473152ffeb1
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-07 00:30:02 +0200

    Update Damaged.vue

--- components/desktop/components/Damaged.vue
+++ components/desktop/components/Damaged.vue
@@ -2,7 +2,7 @@
     <div
           class="datasheet-desktop__section-header"
         >
-        <div class="datasheet-desktop__section-header bg-slate-400 m-3">
+        <div class="datasheet-desktop__section-header  bg-zinc-900 text-zinc-100 rounded-t-lg p-3 m-3">
           <p>Damaged: 1 - {{ damaged.remainingWounds }} wounds remaining </p>
         </div>
         <div class="px-5 py-2">

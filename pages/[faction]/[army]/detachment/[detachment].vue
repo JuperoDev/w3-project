ebd9113a77e1c297d6c4c971435346792f172a8f
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-06 18:58:57 +0200

    Update [detachment].vue

--- pages/[faction]/[army]/detachment/[detachment].vue
+++ pages/[faction]/[army]/detachment/[detachment].vue
@@ -1,14 +1,43 @@
 <template>
-    <div>
-      Detachment: {{ detachment }}
+  <div v-if="collectionData">
+    <h2>{{ collectionData.detachmentName }}</h2>
+    <div v-for="rule in collectionData.detachmentRule" :key="rule.name">
+      <h3>{{ rule.name }}</h3>
+      <p><strong>Lore:</strong> {{ rule.lore }}</p>
+      <p><strong>Description:</strong> {{ rule.description }}</p>
     </div>
-  </template>
-  
-  <script setup>
-  import { useRoute } from "vue-router";
-  
-  const route = useRoute();
-  const faction = route.params.faction;
-  const army = route.params.army;
-  const detachment = route.params.detachment;
-  </script>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+const replaceUnwantedCharacters = (input) => {
+  return input.replace(/[^a-zA-Z0-9-]/g, "-");
+};
+
+const route = useRoute();
+const faction = route.params.faction;
+const army = route.params.army;
+const detachment = route.params.detachment;
+const sanitizedDetachment = replaceUnwantedCharacters(detachment);
+const collectionData = ref(null);
+
+const fetchCollectionData = async () => {
+  try {
+    const res = await fetch(`/faction/${faction}/${army}/detachment/${sanitizedDetachment}.json`);
+    collectionData.value = await res.json();
+    console.log("Fetched Collection Data:", collectionData.value);
+  } catch (error) {
+    console.error("Fetch Collection Data Error:", error);
+  }
+};
+
+onMounted(() => {
+  fetchCollectionData();
+});
+</script>

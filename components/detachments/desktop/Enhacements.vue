07e03709a025ae304d7e8b5c8968c4f6ea396393
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-10 01:05:17 +0200

    Update Enhacements.vue

--- components/detachments/desktop/Enhacements.vue
+++ components/detachments/desktop/Enhacements.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        
+        <div class="enhacements">
+      <h3 class="text-3xl mt-5">Enhacements</h3>
+      <div class="" v-for="enhacement in collectionData.enhacements">
+        <div class="enhacement--element font-semibold">
+          {{ enhacement.name }}
+          {{ enhacement.points }}
+        </div>
+        <div class="enhacement--element italic">
+          {{ enhacement.lore }}
+        </div>
+        <div class="enhacement--element">
+          {{ enhacement.description }}
+        </div>
+      </div>
+      <!-- -------------- -->
+
+
+      <!-- ------------ -->
     </div>
+
 </template>

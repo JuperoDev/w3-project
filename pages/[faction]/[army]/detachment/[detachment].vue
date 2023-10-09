126f40c751206c85482ec70e25c285958fe44d14
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-10 00:34:40 +0200

    update detachment

--- pages/[faction]/[army]/detachment/[detachment].vue
+++ pages/[faction]/[army]/detachment/[detachment].vue
@@ -2,15 +2,33 @@
   <div v-if="collectionData">
     <h2>{{ collectionData.detachmentName }}</h2>
     <div v-for="rule in collectionData.detachmentRule" :key="rule.name">
-
       <h3>Detachment rule:{{ rule.name }}</h3>
       <p><strong>Lore:</strong> {{ rule.lore }}</p>
       <p><strong>Description:</strong> {{ rule.description }}</p>
     </div>
 
-    <div class="enhacements" v-for="enhacement in collectionData.enhacements">
-{{ enhacement }}
+    <!-- ----------------------------- -->
+    <div class="enhacements">
+      <h3 class="text-3xl mt-5">Enhacements</h3>
+      <div class="" v-for="enhacement in collectionData.enhacements">
+        <div class="enhacement--element font-semibold">
+          {{ enhacement.name }}
+          {{ enhacement.points }}
+        </div>
+        <div class="enhacement--element italic">
+          {{ enhacement.lore }}
+        </div>
+        <div class="enhacement--element">
+          {{ enhacement.description }}
+        </div>
+      </div>
+      <!-- -------------- -->
 
+      <h3>Stratagems</h3>
+      <div class="" v-for="stratagem in collectionData.stratagems">
+        {{ stratagem }}
+      </div>
+      <!-- ------------ -->
     </div>
   </div>
   <div v-else>
@@ -35,7 +53,9 @@ const collectionData = ref(null);
 
 const fetchCollectionData = async () => {
   try {
-    const res = await fetch(`/faction/${faction}/${army}/detachment/${sanitizedDetachment}.json`);
+    const res = await fetch(
+      `/faction/${faction}/${army}/detachment/${sanitizedDetachment}.json`
+    );
     collectionData.value = await res.json();
     console.log("Fetched Collection Data:", collectionData.value);
   } catch (error) {
@@ -47,3 +67,9 @@ onMounted(() => {
   fetchCollectionData();
 });
 </script>
+
+<style scoped>
+.enhacement--element {
+  margin-top: 10px;
+}
+</style>

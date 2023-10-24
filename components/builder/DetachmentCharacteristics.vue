2b8cd304b144c98209bffd9ccf47818a70a99ffd
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-24 14:22:15 +0200

    update

---
+++ components/builder/DetachmentCharacteristics.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        asas
+    </div>
+</template>
--- pages/builder/index.vue
+++ pages/builder/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <BuilderSelector/>
+        <BuilderDetachmentCharacteristics/>
     </div>
 </template>

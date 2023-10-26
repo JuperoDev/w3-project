5df3e4adfc14d583aac3626f847762fe658ca210
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-26 22:37:28 +0200

    Update Menu.vue

--- components/rules/Menu.vue
+++ components/rules/Menu.vue
@@ -1,12 +1,37 @@
 <template>
-    <div class="idc">
-        <li><a href="#section1">Section 1</a></li>
-      <li><a href="#section16">Section 16</a></li>
-    </div>
+  <div class="idc">
+    <li><a href="#section1">Section 1</a></li>
+    <li><a href="#section16">Section 16</a></li>
+    <p>Core Concepts</p>
+    <br/>
+    <p>Mission</p>
+    <p>Armies</p>
+    <p>Unit Coherency</p>
+    <p>Engagement Range</p>
+    <p>Battlefield</p>
+    <p>Measuring Distances</p>
+    <p>Determining Visibility</p>
+    <p>Dice</p>
+    <p>Sequencing</p>
+    <br/>
+    <br/>
+    <p>Datasheets and Abilities</p>
+    <br/>
+    <p>Further Rules</p>
+    <br/>
+    <p>Muster your Army</p>
+    <br/>
+    <p>Missions</p>
+    <br/>
+    <p>Rules Commentary</p>
+    <br/>
+  
+
+  </div>
 </template>
 <style scoped>
-.idc{
-    position: fixed;
-    background-color: aliceblue;
+.idc {
+  position: fixed;
+  background-color: aliceblue;
 }
 </style>

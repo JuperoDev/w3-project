80fbdeca47489de99c7d2de3115ae33eedbaef13
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-27 02:42:16 +0200

    Update Menu.vue

--- components/rules/Menu.vue
+++ components/rules/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="idc">
     <li><a href="#section1">Section 1</a></li>
-    <li><a href="#section16">Section 16</a></li>
+    <li><nuxt-link to="#section16">Section 16</nuxt-link></li>
     <h4>Core Concepts</h4>
-    <br />
+
     <h5>Mission</h5>
     <h5>Armies</h5>
     <h5>Unit Coherency</h5>
@@ -13,26 +13,23 @@
     <h5>Determining Visibility</h5>
     <h5>Dice</h5>
     <h5>Sequencing</h5>
-    <br />
-    <br />
+
     <h4>The Battle Round</h4>
-    <br />
+
     <h5>Datasheets and Abilities</h5>
     <h5>Datasheets and Abilities</h5>
-    <br />
-    <br />
+
     <h4>Datasheets and Abilities</h4>
-    <br />
+
     <h4>Datasheets</h4>
     <h4>Deployment Abilities</h4>
     <h5>Further Rules</h5>
-    <br />
+
     <h4>Muster your Army</h4>
-    <br />
+
     <h4>Missions</h4>
-    <br />
+
     <h4>Rules Commentary</h4>
-    <br />
   </div>
 </template>
 <style scoped>
@@ -41,16 +38,18 @@
   /* background-color: rgb(239, 246, 252); */
 }
 
-h5 {
+h4 {
   padding: 6px 6px 6px 20px;
+}
+h5 {
+  padding: 6px 6px 6px 50px;
   color: rgb(120, 120, 120);
   /* border: 1px solid #efefef; */
 }
 
-
-h5:hover{
-background-color:  rgb(220, 220, 220);
-/* border: 1px solid #ffffff; */
-color: rgb(64, 64, 64);
+h5:hover {
+  background-color: rgb(241, 241, 241);
+  /* border: 1px solid #ffffff; */
+  color: rgb(64, 64, 64);
 }
 </style>

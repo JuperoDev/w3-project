1ba44e6d95f57d1546e85429c42f03a0dbb92849
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-27 02:28:50 +0200

    Update Menu.vue

--- components/rules/Menu.vue
+++ components/rules/Menu.vue
@@ -2,36 +2,55 @@
   <div class="idc">
     <li><a href="#section1">Section 1</a></li>
     <li><a href="#section16">Section 16</a></li>
-    <p>Core Concepts</p>
-    <br/>
-    <p>Mission</p>
-    <p>Armies</p>
-    <p>Unit Coherency</p>
-    <p>Engagement Range</p>
-    <p>Battlefield</p>
-    <p>Measuring Distances</p>
-    <p>Determining Visibility</p>
-    <p>Dice</p>
-    <p>Sequencing</p>
-    <br/>
-    <br/>
-    <p>Datasheets and Abilities</p>
-    <br/>
-    <p>Further Rules</p>
-    <br/>
-    <p>Muster your Army</p>
-    <br/>
-    <p>Missions</p>
-    <br/>
-    <p>Rules Commentary</p>
-    <br/>
-  
-
+    <h4>Core Concepts</h4>
+    <br />
+    <h5>Mission</h5>
+    <h5>Armies</h5>
+    <h5>Unit Coherency</h5>
+    <h5>Engagement Range</h5>
+    <h5>Battlefield</h5>
+    <h5>Measuring Distances</h5>
+    <h5>Determining Visibility</h5>
+    <h5>Dice</h5>
+    <h5>Sequencing</h5>
+    <br />
+    <br />
+    <h4>The Battle Round</h4>
+    <br />
+    <h5>Datasheets and Abilities</h5>
+    <h5>Datasheets and Abilities</h5>
+    <br />
+    <br />
+    <h4>Datasheets and Abilities</h4>
+    <br />
+    <h4>Datasheets</h4>
+    <h4>Deployment Abilities</h4>
+    <h5>Further Rules</h5>
+    <br />
+    <h4>Muster your Army</h4>
+    <br />
+    <h4>Missions</h4>
+    <br />
+    <h4>Rules Commentary</h4>
+    <br />
   </div>
 </template>
 <style scoped>
 .idc {
   position: fixed;
-  background-color: aliceblue;
+  /* background-color: rgb(239, 246, 252); */
+}
+
+h5 {
+  padding: 6px 6px 6px 20px;
+  color: rgb(120, 120, 120);
+  /* border: 1px solid #efefef; */
+}
+
+
+h5:hover{
+background-color:  rgb(220, 220, 220);
+/* border: 1px solid #ffffff; */
+color: rgb(64, 64, 64);
 }
 </style>

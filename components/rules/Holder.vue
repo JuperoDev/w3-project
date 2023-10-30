6a10d85e1c13fad1963771d5a725027a3db8044f
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-10-30 01:09:13 +0100

    rules components refactor

--- components/rules/Holder.vue
+++ components/rules/Holder.vue
@@ -1,167 +1,11 @@
 <template>
   <div>
     <!-- Core concepts -->
-    <div class="section__core-concepts">
-      <h3 id="section1" class="menu-holder__header">CORE CONCEPTS</h3>
-    <h5>
-      This section introduces various rules terms that you will find throughout
-      the Core Rules and beyond. These key concepts form the basis of the
-      Warhammer 40,000 rules, and are essential for every kind of battle.
-    </h5>
-    <h4>MISSIONS</h4>
-    <p>
-      To play a game of Warhammer 40,000, you must first select a mission. The
-      mission will tell you how to muster your armies, create your battlefield
-      and deploy your forces on the tabletop. It will also tell you any special
-      rules that apply to the battle, and (most importantly!) what you need to
-      do to win. You can find out more about missions here.
-    </p>
-    <h4>ARMIES</h4>
-    <p>
-      Each player in a game of Warhammer 40,000 commands an army of Citadel
-      miniatures, hereafter referred to as models. The mission you have selected
-      will guide you as to how big your army should be.
-    </p>
-
-    <div class="menu-holder_highlight">
-      <ul>
-        <li><b>Army:</b> All of the models under your command.</li>
-      </ul>
-    </div>
-
-    <h4>Units</h4>
-    <p>
-      Models move and fight in units. A unit can have one or more models from a
-      single datasheet (see below). Models and units from the same army are said
-      to be friendly in relation to each other. Models and units from your
-      opponent’s army are referred to as enemy models and units. If a rule
-      affects ‘models’ or ‘units’ without specifying that they are friendly or
-      enemy, that rule affects all models or all units, regardless of whose army
-      they are in.
-    </p>
-
-    <div class="menu-holder_highlight">
-      <ul>
-        <li><b>Unit:</b> One or more models from the same datasheet.</li>
-        <li>
-          <b>Friendly Models/Units:</b> All models/units from the same army.
-        </li>
-        <li>
-          <b>Enemy Models/Units:</b> All models/units from your opponent’s army.
-        </li>
-      </ul>
-    </div>
-    <h4>Datasheets</h4>
-    <p>
-      The rules you will need to use your models in games are presented on
-      datasheets. Each unit has a datasheet; you will need the datasheets for
-      all the units from your army. You can find out more about datasheets here.
-    </p>
-
-    <h4>KEYWORDS</h4>
-    <p>
-      All datasheets have a list of keywords, separated into Faction keywords
-      and other keywords. Faction keywords can be used to help you decide which
-      models to include in your army, but otherwise both sets of keywords are
-      functionally the same. In either case, keywords appear in KEYWORD BOLD.
-      Keywords are sometimes linked to (or ‘tagged’ by) a rule. For example, a
-      rule might say that it applies to INFANTRY units. This means it only
-      applies to units that have the INFANTRY keyword on their datasheet. The
-      pluralisation (or otherwise) of keywords does not affect which units the
-      rule in question applies to.
-    </p>
-
-    <h4>Engagement Range</h4>
-    <p>
-      Engagement Range represents the zone of threat that models present to
-      their enemies. While a model is within 1" horizontally and 5" vertically
-      of an enemy model, those models – and their units – are within Engagement
-      Range of each other.
-    </p>
-
-    <p>
-      Models cannot be set up or end a Normal, Advance or Fall Back move within
-      Engagement Range of any enemy models. If for any reason a model cannot
-      meet this condition, that model is destroyed.
-    </p>
-    <div class="menu-holder_highlight">
-      <ul>
-        <li>
-          <b>Engagement Range:</b> Within 1" horizontally and 5" vertically.
-        </li>
-
-        <li>Models cannot be set up or end a Normal, Advance or Fall Back move
-          within Engagement Range of any enemy models.
-        </li>
-      </ul>
-    </div>
-
-    <div class="hight-ligt-area">
-      <h4>UNIT COHERENCY</h4>
-
-      <p>
-        A unit that contains more than one model must be set up and end any kind of move as a single group, with all of its models within 2" horizontally and 5" vertically of at least one other model from that unit. While a unit has seven or more models, all of its models must instead be set up and end any kind of move within 2" horizontally and 5" vertically of at least two other models from that unit. This is called Unit Coherency.
-      </p>
-      <br/>
-      <p>
-        If for any reason a model cannot be set up in Unit Coherency, that model is destroyed. If a unit cannot end a move in Unit Coherency, it cannot make that move and its models are returned to their previous positions.
-      </p>
-      <br/>
-      <p>
-        At the end of every turn, each player must remove models, one at a time, from any of the units from their armies that are no longer in
-      </p>
-      <br/>
-      <p>
-        Unit Coherency, until only a single group of models from each of those units remains in play and in Unit Coherency. Models removed in this way count as having been destroyed, but they never trigger any rules that apply when a model is destroyed.
-      </p>
-
-
-      <div class="menu-holder_highlight">
-        <ul>
-          <li>
-            <b>Unit Coherency: </b> Within 2" horizontally and 5" vertically of:
-            <p>- One other model from the same unit (in units of 2-6 models).</p>
-            <p>- Two other models from the same unit (in units of 7+ models).</p>
-          </li>
-<br/>
-          <li>At the end of every turn, if a unit is not in Unit Coherency, the controlling player must remove models until that unit is in Unit Coherency again.
-          </li>
-        </ul>
-        <br/>
-      </div>
-      <img src="https://wahapedia.ru/wh40k10ed/img/UnitCoherency.png" alt="Unit coherency" class="menu__img-place-holder"/>
-      <div class="menu-holder__subtitles--xl flex">
-        <div class="menu-holder__subtitles--red-square"><p>A</p></div><p>Each model in this Sternguard Veterans unit of five models is within 2" of at least one other model in the unit. This unit is therefore in Unit Coherency.</p>
-      </div>
-    </div>
-
-    </div>
+   <RulesSectionsCoreConcepts/>
     
 <!-- BATTLE ROUND  -->
-    <div class="section__battle-round">
-      <h3 id="section2" class="menu-holder__header">The Battle Round</h3>
-    <h5>
-      Warhammer 40,000 is played in a series of battle rounds. In each battle round, both players have a turn. The same player always takes the first turn in each battle round – the mission you are playing will tell you which player this is. Each turn consists of a series of phases, which must be resolved in the following order.
-    </h5>
-
-    <h4>Command</h4>
-    <p>
-      At the start of your Command phase, before doing anything else, both players gain 1CP. Command points are a strategic resource that you can spend during the battle to use Stratagems.
-    </p>
-
-    <p>Then, if you have any other rules that need to be resolved in the Command phase, you do so now before progressing to the Battle-shock step.</p>
-
-    <div class="menu-holder_highlight">
-      <ul>
-        <li>Both players gain 1CP.</li>
-        <li>Resolve any other rules that occur in the Command phase.</li>
-      </ul>
-    </div>
-
-    
-
-    </div>
-
+   
+<RulesSectionsBattleRound/>
 
     <!-- DATASHEET AND ABILITIES  -->
 
@@ -207,7 +51,7 @@ li::before {
   font-size: 15px;
 }
 
-.hight-ligt-area{
+.high-light-area{
 background-color: rgba(255, 246, 230, 0.523);
 }
 
---
+++ components/rules/sections/BattleRound.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="section__battle-round">
+    <h3 id="section2" class="menu-holder__header">The Battle Round</h3>
+    <h5>
+      Warhammer 40,000 is played in a series of battle rounds. In each battle
+      round, both players have a turn. The same player always takes the first
+      turn in each battle round – the mission you are playing will tell you
+      which player this is. Each turn consists of a series of phases, which must
+      be resolved in the following order.
+    </h5>
+
+    <h4>Command</h4>
+    <p>
+      At the start of your Command phase, before doing anything else, both
+      players gain 1CP. Command points are a strategic resource that you can
+      spend during the battle to use Stratagems.
+    </p>
+
+    <p>
+      Then, if you have any other rules that need to be resolved in the Command
+      phase, you do so now before progressing to the Battle-shock step.
+    </p>
+
+    <div class="menu-holder_highlight">
+      <ul>
+        <li>Both players gain 1CP.</li>
+        <li>Resolve any other rules that occur in the Command phase.</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h4 {
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+.menu-holder_highlight {
+  background-color: rgb(237, 237, 237);
+  padding: 20px;
+  /* min-width: 500px; */
+  margin: 20px;
+}
+
+ ul {
+  /* min-width: 700px; */
+  list-style: none;
+}
+
+li::before {
+  content: "■";
+  list-style-type: square;
+  color: rgb(218, 0, 0);
+  display: inline-block;
+  width: 1em;
+  margin-right: 1em;
+
+  font-size: 15px;
+}
+
+.high-light-area{
+background-color: rgba(255, 246, 230, 0.523);
+}
+
+.menu__img-place-holder{
+margin: 0 auto;
+}
+
+.menu-holder__subtitles--red-square{
+  height: 30px;
+  width: 40px;
+  background-color: rgb(189, 9, 9);
+  
+  margin-right: 20px;
+}
+.menu-holder__subtitles--red-square p{
+ margin-left: 8px;
+ margin-top: 2px; 
+ font-weight: 700;
+}
+
+.menu-holder__subtitles--xl{
+  background-color: rgb(45, 45, 45);
+  padding: 20px;
+  color: rgb(246, 248, 250);
+  margin: 0 auto;
+  max-width: 822px;
+}
+
+.menu-holder__header{
+  font-size: 30px;
+  font-weight: 700;
+  margin: 10px;
+}
+</style>

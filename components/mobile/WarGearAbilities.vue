128f3aa4d9b9037335067e9c14f644bba450cef4
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-12-24 13:06:55 +0100

    Update WarGearAbilities.vue

--- components/mobile/WarGearAbilities.vue
+++ components/mobile/WarGearAbilities.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="abilities-container p-3">
+      
+      <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3 m-3">
+        <p>Wargear Abilities</p>
+      </div>
+      
        {{ warGearAbilities }} 
 
         

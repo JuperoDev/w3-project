7c77260fce714215e2701693644d1e82a500b192
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-12-24 18:17:15 +0100

    Update index.vue

--- pages/[faction]/[army]/[collection]/index.vue
+++ pages/[faction]/[army]/[collection]/index.vue
@@ -37,7 +37,7 @@
 
     <v-expansion-panels>
       <!-- Ranged weapons -->
-      <v-expansion-panel v-if="rangedWeapons">
+      <v-expansion-panel v-if="rangedWeapons.length>1">
         <v-expansion-panel-title>
           <div class="uppercase font-semibold">Ranged weapons</div>
         </v-expansion-panel-title>

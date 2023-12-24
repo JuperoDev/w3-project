9de01c62f24e82f744832c18c48883c0b1e76414
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-12-24 13:06:18 +0100

    update

--- components/desktop/components/WargearAbility.vue
+++ components/desktop/components/WargearAbility.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="datasheet-desktop__section-header">
-      <!-- <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3 m-3">
+      <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3 m-3">
         <p>Wargear Abilities</p>
       </div>
+      <!--
       <div class="abilities-container p-3">
         <div class="other-abilities-container" v-if="abilities && abilities.warGearAbilities && abilities.warGearAbilities.length > 0">
           <div v-for="(warGearAbility, index) in abilities.warGearAbilities" :key="'misc-' + index">

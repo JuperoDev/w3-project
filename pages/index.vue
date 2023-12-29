760c8b6f96eff5a56c9541c7e10aa0543842d49c
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-12-29 20:30:41 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,16 +12,16 @@
 
       <div class="button__container my-5 ">
         <div class="army__card font-bebas">
-          <!-- <nuxt-link to="/xenos/aeldari">
+          <nuxt-link to="/imperium/adepta-sororitas">
             <div class="army__card_image">
               <img
-                src="/icons/aeldari-symbol.png"
+                src="/icons/sororitas-symbol.png"
               />
             </div>
             <div class="army__card_title title-white uppercase">
-              <p>Aeldari</p>
+              <p>Adeptas Sororitas</p>
             </div>
-          </nuxt-link> -->
+          </nuxt-link> 
         </div>
       </div>
 

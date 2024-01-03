5cfaf0ac3d4cc77a44b16cb4cdf43d3546dd777d
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-01-03 17:08:30 +0100

    minor bug fix. test in prod

--- stores/UnitStore.js
+++ stores/UnitStore.js
@@ -3,34 +3,29 @@ import { defineStore } from "pinia";
 export const useUnitStore = defineStore("test", {
   state: () => ({
     message: "hello",
-    elements: [], // Original array to store elements
-    info: [{parentUnit:"termagants","unitUrl":"xenos/tyranids/termagants"}], // Array to store objects with "element," "parentUnit," and "unitURL"
+    elements: [], // Array to store elements,
+    info:[]
   }),
 
   // Action to add or remove elements
   actions: {
     addOrRemoveElement(elementName, isAdd) {
       if (isAdd) {
-        // Check if the elementName is not already in the info array
-        if (!this.isElementInInfo(elementName)) {
-          this.elements.push(elementName); // Add to original elements array
-          this.info.push({ element: elementName, parentUnit: elementName, unitURL: "" });
+        // Check if the elementName is not already in the array
+        if (!this.elements.includes(elementName)) {
+          this.elements.push(elementName);
         }
       } else {
-        const index = this.info.findIndex((item) => item.parentUnit === elementName);
+        const index = this.elements.indexOf(elementName);
         if (index !== -1) {
-          this.info.splice(index, 1);
-          const elementIndex = this.elements.indexOf(elementName);
-          if (elementIndex !== -1) {
-            this.elements.splice(elementIndex, 1);
-          }
+          this.elements.splice(index, 1);
         }
       }
     },
   },
   getters: {
     isElementInList: (state) => (elementName) => {
-      return state.info.some((item) => item.parentUnit === elementName);
+      return state.elements.includes(elementName);
     },
   },
   persist: true,

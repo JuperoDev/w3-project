a5034f0ba192edc02a105651a42d7d5cb5c470bb
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-01-05 13:52:54 +0100

    go back button

--- components/generalPurpose/BackButton.vue
+++ components/generalPurpose/BackButton.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
-      <v-btn @click="goBack" >Go Back</v-btn>
-    </div>
-  </template>
-  
-  <script>
+  <div>
+    <v-btn @click="goBack">Go Back</v-btn>
+  </div>
+</template>
 
-  
-  export default {
-    setup() {
-      const goBack = () => {
+<script>
+import { useRoute, useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+
+    const goBack = () => {
+      if (route.fullPath === '/xenos/tyranids') {
+        // If the current route is /xenos/tyranids, navigate to the root URL
+        router.push('/');
+      } else {
+        // Otherwise, go back in the route history
         window.history.back();
-      };
-  
-      return {
-        goBack,
-      };
-    },
-  };
-  </script>
-  
-  
+      }
+    };
+
+    return {
+      goBack,
+    };
+  },
+};
+</script>

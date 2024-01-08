bd2ec48c5c0ec77c633da9767ffb9c507b5ba676
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-01-08 16:02:20 +0100

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      
+      <nuxt />
+    </div>
+  </template>
+  
+  <script setup>
+  import { useHead } from '@nuxt/content'
+  
+  useHead({
+    title: 'WH40K Data',
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap'
+      }
+    ]
+  })
+  </script>
+  

2ab70158d0b7f121aeaf191cbb379cf1f48e789c
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-01-11 23:54:11 +0100

    Update FavDialog.vue

--- components/generalPurpose/FavDialog.vue
+++ components/generalPurpose/FavDialog.vue
@@ -2,7 +2,7 @@
   <div>
     <v-dialog width="500">
       <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" text="Fav Units"> </v-btn>
+        <v-btn v-if="unitStore.elements.length > 0" v-bind="props" text="Fav Units"> </v-btn>
       </template>
 
       <template v-slot:default="{ isActive }">
@@ -13,7 +13,9 @@
                 <div class="fav-unit__list py-2 my-1" v-for="(element, index) in unitStore.elements" :key="index">
                   <v-btn>
                     <nuxt-link :to="generateLink(unitStore.info[index])">{{ element }}</nuxt-link>
-                    
+                  </v-btn>
+                  <v-btn icon @click="removeFavoriteUnit(index)">
+                    <v-icon>mdi-close</v-icon>
                   </v-btn>
                 </div>
               </ul>
@@ -44,5 +46,13 @@ const generateLink = (info) => {
   const urlParts = info.split('/'); // Split the URL into parts
   const filteredParts = urlParts.filter(part => part !== 'faction' && part !== 'collection'); // Remove 'faction' and 'collection' if present
   return `${baseURL}${filteredParts.join('/')}`;
+}
+
+const removeFavoriteUnit = (index) => {
+  // Remove the favorite unit at the specified index from unitStore.elements and unitStore.info
+  if (index >= 0 && index < unitStore.elements.length) {
+    unitStore.elements.splice(index, 1);
+    unitStore.info.splice(index, 1);
+  }
 }
 </script>

823aa05cf2d9d5899d5055a74ff5139db2be4c9d
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-02-10 13:28:22 +0100

    Update UnitStore.js

--- stores/UnitStore.js
+++ stores/UnitStore.js
@@ -2,7 +2,6 @@ import { defineStore } from "pinia";
 
 export const useUnitStore = defineStore("test", {
   state: () => ({
-    message: "hello",
     elements: [], // Array to store elements,
     info: [], // Array to store info (url)
   }),

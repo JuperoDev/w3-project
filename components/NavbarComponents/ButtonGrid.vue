6716ebfb283140cf1c2f96d40dd719a1f86cb9f8
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-02-12 22:19:42 +0100

    update search bar

--- components/NavbarComponents/ButtonGrid.vue
+++ components/NavbarComponents/ButtonGrid.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="p-5 bg-zinc-900 navigation-bar flex flex-row">
+    <div class="p-5  bg-zinc-900 navigation-bar">
+    <div class=" flex flex-row">
        
          <GeneralPurposeBackButton class="mx-2"/> 
-         <GeneralPurposeSearchBar class="mx-2"/>
+         
          <GeneralPurposeFavDialog class="mx-2"/>
         
     </div>
+    <GeneralPurposeSearchBar class="m-2"/>
+</div>
 </template>
--- components/generalPurpose/SearchBar.vue
+++ components/generalPurpose/SearchBar.vue
@@ -1,93 +1,117 @@
 <template>
-    <div class="custom-select">
-      <input
-        v-model="searchQuery"
-        type="text"
-        placeholder="Search"
-        class="bg-gray-100"
-        @input="onSearch"
-        @blur="onBlur"
+  <div class="custom-select">
+    <input
+      v-model="searchQuery"
+      type="text"
+      placeholder="Search"
+      class="bg-gray-100"
+      @input="onSearch"
+      @blur="onBlur"
+    >
+    <ul v-show="isOpen && searchQuery.length >= 3" class="options capitalize">
+      <li
+        v-for="(option, index) in filteredOptions"
+        :key="index"
+        @click="navigate(option)"
       >
-      <ul v-show="isOpen && searchQuery.length >= 3" class="options">
-        <li
-          v-for="(option, index) in filteredOptions"
-          :key="index"
-          @click="selectOption(option)"
-        >
-          {{ option }}
-        </li>
-        <li v-if="moreElementsMessage" class="more-elements">
-          {{ moreElementsMessage }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, computed, watch } from 'vue';
-  
-  const selected = ref(null);
-  const options = [
-    'Broodlord', 'Deathleaper', 'Hive Tyrant', 'Neurotyrant', 'Old One Eye',
-    'Parasite Of Mortrex', 'Tervigon', 'The Swarmlord', 'Winged Hive Tyrant',
-    'Winged Tyranid Prime', 'Battleline', 'Gargoyles', 'Hormagaunts', 'Termagants',
-    'Dedicated Transports', 'Tyrannocyte', 'Other', 'Barbgaunts', 'Biovores',
-    'Carnifexes', 'Exocrine', 'Genestealers', 'Harpy', 'Haruspex', 'Hive Crone',
-    'Hive Guard', 'Lictor', 'Maleceptor', 'Mawloc', 'Mucolid Spores', 'Neurogaunts',
-    'Neurolictor', 'Norn Assimilator', 'Norn Emissary', 'Psychophage', 'Pyrovores',
-    'Raveners', 'Ripper Swarms', 'Screamer-killer', 'Spore Mines', 'Sporocyst',
-    'Toxicrene', 'Trygon', 'Tyranid Warriors With Melee Bio-weapons',
-    'Tyranid Warriors With Ranged Bio-weapons', 'Tyrannofex', 'Tyrant Guard',
-    'Venomthropes', 'Von Ryan’s Leapers', 'Zoanthropes'
-  ];
-  const searchQuery = ref('');
-  const isOpen = ref(false);
-  const moreElementsMessage = ref('');
-  
-  const filteredOptions = computed(() => {
-    if (searchQuery.value.length < 3) {
-      return [];
-    }
-  
-    const filtered = options.filter(option =>
-      option.toLowerCase().includes(searchQuery.value.toLowerCase())
-    );
-  
-    if (filtered.length > 5) {
-      moreElementsMessage.value = `There are ${filtered.length - 5} more elements`;
-      return filtered.slice(0, 5);
-    } else {
-      moreElementsMessage.value = '';
-      return filtered;
-    }
-  });
-  
-  const onSearch = () => {
-    isOpen.value = true;
+        {{ option }}
+      </li>
+      
+      <li v-if="moreElementsMessage" class="more-elements">
+        {{ moreElementsMessage }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, watch } from 'vue';
+import searchResults from "./searchResults.json";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const selected = ref(null);
+const searchQuery = ref('');
+const isOpen = ref(false);
+const moreElementsMessage = ref('');
+const options = [];
+
+// Flatten the nested object structure into a single array
+for (const category in searchResults) {
+  for (const subCategory in searchResults[category]) {
+    options.push(...searchResults[category][subCategory]);
+  }
+}
+
+const filteredOptions = computed(() => {
+  if (searchQuery.value.length < 3) {
+    return [];
+  }
+
+  const filtered = options.filter(option =>
+    option.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+
+  if (filtered.length > 8) {
+    moreElementsMessage.value = `There are ${filtered.length - 8} more elements`;
+    return filtered.slice(0, 8);
+  } else {
     moreElementsMessage.value = '';
-  };
-  
-  const onBlur = () => {
-    // Delay hiding the options to allow click events on options
-    setTimeout(() => {
-      isOpen.value = false;
-    }, 200);
-  };
-  
-  const selectOption = (option) => {
-    selected.value = option;
+    return filtered;
+  }
+});
+
+const onSearch = () => {
+  isOpen.value = true;
+  moreElementsMessage.value = '';
+};
+
+const onBlur = () => {
+  // Delay hiding the options to allow click events on options
+  setTimeout(() => {
     isOpen.value = false;
-  };
-  
-  watch(selected, (newValue) => {
-    searchQuery.value = newValue;
-  });
-  </script>
-  
-  <style>
+  }, 200);
+};
+
+const selectOption = (option) => {
+  selected.value = option;
+  isOpen.value = false;
+};
+
+const navigate = (option) => {
+  const currentRoute = router.currentRoute.value;
+  const currentCategory = currentRoute.params.category;
+  const currentSubCategory = currentRoute.params.subCategory;
+
+  for (const category in searchResults) {
+    for (const subCategory in searchResults[category]) {
+      if (searchResults[category][subCategory].includes(option)) {
+        if (currentCategory === category && currentSubCategory === subCategory) {
+          const formattedOption = option.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-');
+          router.push(`/${category}/${subCategory}/${formattedOption}`);
+        } else {
+          const formattedOption = option.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-');
+          router.push(`/${category}/${subCategory}/${formattedOption}`);
+        }
+        return;
+      }
+    }
+  }
+};;
+
+watch(selected, (newValue) => {
+  searchQuery.value = newValue;
+});
+</script>
+  <style scoped>
+
+  ul{
+    z-index: 999;
+  }
     .custom-select {
       position: relative;
-      width: 200px; /* adjust width as needed */
+      width: 80%; /* adjust width as needed */
+      
     }
     input[type="text"] {
       width: 100%;

42d9b0b222c23edd5df891db06cb6c1f238fec5c
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-02-14 00:20:41 +0100

    update for chaos

--- weaponFormat.py
+++ weaponFormat.py
@@ -30,5 +30,5 @@ def process_files(folder_path):
                 merge_stats(file_path, file_path)
 
 if __name__ == "__main__":
-    folder_path = r"C:\Users\JPR\Documents\GitHub\w3-project\public\faction\chaos\death-guard\collection"
+    folder_path = r"C:\Users\JPR\Documents\GitHub\w3-project\public\faction\chaos\chaos-space-marines\collection"
     process_files(folder_path)

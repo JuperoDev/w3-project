390f8c2de1e56701d47600516e9fb794dfe6abc0
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-02-23 01:59:50 +0100

    fix

--- pages/[faction]/index.vue
+++ pages/[faction]/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
    
-    <NavbarComponentsFactionButtonGrid/>
+    <NavbarComponentsArmyButtonGrid/>
 
     <div v-if="factionData && factionData.army">
       <div v-for="armyName in factionData.army" :key="armyName" class="m-5">

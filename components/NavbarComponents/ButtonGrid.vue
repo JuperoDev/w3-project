b535e5af3d6f4fff7838c4437b7bff717ff22c0d
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-03-21 23:10:13 +0100

    update

--- components/NavbarComponents/ButtonGrid.vue
+++ components/NavbarComponents/ButtonGrid.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="p-5  bg-zinc-900 navigation-bar">
     <div class=" flex flex-row">
-       
-         <GeneralPurposeBackButton class="mx-2"/> 
+        <GeneralPurposeBackButton class="mx-2"/> 
+        
          
          <GeneralPurposeFavDialog class="mx-2"/>
         
     </div>
     <GeneralPurposeSearchBar class="m-2"/>
+    <!-- <GeneralPurposePopMenu/> -->
 </div>
 </template>

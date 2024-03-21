bc6caf66f732c235d7e1f6ef631f6ac1c2f9f3d5
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-03-22 00:16:52 +0100

    update

--- components/detachments/desktop/Enhacements.vue
+++ components/detachments/desktop/Enhacements.vue
@@ -2,12 +2,14 @@
         <div class="enhacements px-15">
       <h3 class="text-2xl font-bebas mt-5 px-3">Enhacements</h3>
       
-      <div class="grid grid-cols-1 md:grid-cols-2 md:gap-10 ">
+      <div class="grid grid-cols-1 mt-5 md:grid-cols-2 md:gap-10 ">
       <div class="" v-for="enhacement in enhacements">
-        <div class="enhacement--element font-semibold mb-4">
-          {{ enhacement.name }}
-          <span> ............ </span>
-          {{ enhacement.points }} <span> pts </span>
+        <div class="enhacement--element font-semibold mb-4 flex">
+          <div class="w-2/5"> {{ enhacement.name }}</div>
+          <div class="w-2/5 "> </div>
+          <div class="w-1/5"> {{ enhacement.points }} <span> pts </span></div>
+          
+          
         </div>
         <div class="enhacement--element italic mb-2">
           {{ enhacement.lore }}

809ec20cfd9642c70e0e015a91cf3cdf20dcd405
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-03-21 17:42:06 +0100

    Update StrataCard.vue

--- components/detachments/StrataCard.vue
+++ components/detachments/StrataCard.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <div class="bg-white shadow-lg">
-        
       <div class="flex">
         <div class="w-16 bg-white"></div>
-        <div class="red-stripe border-solid border-l-8 border-red-500 ml-2 mr-5 bg-red-500 relative">
+        <div
+          class="red-stripe border-solid border-l-8 ml-2 mr-5  relative"
+          :class="{
+                'border-red-600': stratagem.color === 'red',
+                'border-blue-600': stratagem.color === 'blue',
+                'border-green-600': stratagem.color === 'green',
+              }"
+          >
           <div class="absolute top-1/2 left-0 transform -translate-y-1/2">
             <div
-              class="mt-10 -ml-1 w-10 h-10 bg-red-500 transform -translate-x-1/2 -translate-y-1/2 rotate-45 p-5"
+              class="mt-10 -ml-1 w-10 h-10  transform -translate-x-1/2 -translate-y-1/2 rotate-45 p-5"
+              :class="{
+                'bg-red-600': stratagem.color === 'red',
+                'bg-blue-600': stratagem.color === 'blue',
+                'bg-green-600': stratagem.color === 'green',
+              }"
             >
               <div
                 class="transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-zinc-100"
@@ -15,41 +26,67 @@
             </div>
 
             <div
-              class="mt-10 -ml-1 w-10 h-10 bg-red-500 transform -translate-x-1/2 -translate-y-1/2 rotate-45 p-5"
+              class="mt-10 -ml-1 w-10 h-10  transform -translate-x-1/2 -translate-y-1/2 rotate-45 p-5"
+              :class="{
+                'bg-red-600': stratagem.color === 'red',
+                'bg-blue-600': stratagem.color === 'blue',
+                'bg-green-600': stratagem.color === 'green',
+              }"
             >
               <div
                 class="transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-zinc-100"
               >
-                <div class="transform -rotate-45">{{stratagem.cp}}cp</div>
+                <div class="transform -rotate-45 uppercase "><p>{{ stratagem.cp }}cp</p>  </div>
               </div>
             </div>
           </div>
         </div>
         <div class="p-4 ml-5">
-          <h2
-            class="text-lg mb-2 font-bold text-red-600 border-solid border-b-2 border-red-600 uppercase"
-          >
-            {{stratagem.name}}
-          </h2>
+            <h2 :class="'text-lg mb-2 font-bold text-' + stratagem.color + '-600 border-solid border-b-2 border-' + stratagem.color + '-600 uppercase'">
+  {{ stratagem.name }}
+</h2>
+
           <p
             class="text-gray-600 border-dotted border-b-2 border-gray-500 mb-2 capitalize"
           >
-             {{stratagem.subindex}}
+            {{ stratagem.subindex }}
           </p>
           <p class="italic">
-            {{stratagem.lore}}
+            {{ stratagem.lore }}
           </p>
           <p class="my-2">
-            <span class="text-red-600"><strong>When: </strong></span>{{stratagem.when}}
+            <span
+              :class="{
+                'text-red-600': stratagem.color === 'red',
+                'text-blue-600': stratagem.color === 'blue',
+                'text-green-600': stratagem.color === 'green',
+              }"
+              ><strong>When: </strong></span
+            >{{ stratagem.when }}
           </p>
 
           <p class="my-2">
-            <span class="text-red-600"><strong>Target: </strong></span>
-            {{stratagem.target}}
+            <span
+              :class="{
+                'text-red-600': stratagem.color === 'red',
+                'text-blue-600': stratagem.color === 'blue',
+                'text-green-600': stratagem.color === 'green',
+              }"
+              ><strong>Target: </strong></span
+            >
+            {{ stratagem.target }}
           </p>
           <p class="my-2">
-            <span class="text-red-600"><strong>Effect: </strong></span>
-            {{stratagem.effect}}
+            <span
+              :class="{
+                'text-red-600': stratagem.color === 'red',
+                'text-blue-600': stratagem.color === 'blue',
+                'text-green-600': stratagem.color === 'green',
+              }"
+            >
+              <strong>Effect: </strong></span
+            >
+            {{ stratagem.effect }}
           </p>
         </div>
       </div>

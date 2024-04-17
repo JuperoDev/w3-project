5907b990f6afc37e564794f492a5360ce4435a9d
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-04-17 15:21:07 +0200

    Update ArmyRules.vue

--- components/desktop/armyComponents/ArmyRules.vue
+++ components/desktop/armyComponents/ArmyRules.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="army-rules-container">
-    <h2 class="text-3xl uppercase px-10">{{ army }} Army Rules</h2>
+    <h2 class="text-3xl uppercase px-10"> Army Rules</h2>
     <div class="army-rules__grid grid grid-cols-1 md:grid-cols-2 gap-4">
       <div class="army-rules__grid-element p-10" v-for="rule in armyRules" :key="rule.id">
-        <h4 class="text-2xl uppercase">{{ rule.name }}</h4>
+        <h4 class="text-2xl uppercase py-2">{{ rule.name }}</h4>
         <p class="italic" v-html="rule.lore"></p>
         <p class="mt-5" v-html="rule.description"></p>
       </div>
     </div>
+
+    
   </div>
 </template>
 
@@ -22,3 +24,14 @@ const props = defineProps({
 const route = useRoute();
 const army = route.params.army;
 </script>
+
+<style scoped>
+h2 {
+  font-size: 40px;
+  font-weight: 500;
+  color: #191919;
+  text-transform: uppercase;
+  
+}
+
+</style>

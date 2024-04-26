2cfeb55294dfec96f92cc424f498e8001f983559
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-04-26 20:37:44 +0200

    update custodes && style

--- components/detachments/desktop/Stratagems.vue
+++ components/detachments/desktop/Stratagems.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="m-3 px-15">
+  <div class="m-3 px-2 md:px-15">
     <h3 class="text-2xl font-bebas mt-5">Stratagems</h3>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 md:gap-4">
       <div v-for="stratagem in stratagems">
         <DetachmentsStrataCard :stratagem="stratagem" />
       </div>

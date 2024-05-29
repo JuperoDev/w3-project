533b7336cdd4f2af67ec8f2c706518b8d61048ee
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-05-30 01:20:36 +0200

    dangerous commit

--- pages/simulator/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="simulator-container grid grid-cols-2 gap-4">
-    <div>
-        <SimulatorPick :attack-role="true"/>
-        <SimulatorPick :attack-role="false"/>
-        
-    </div>
-</div>
-</template>

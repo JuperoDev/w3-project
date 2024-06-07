d45693af4f6cab05f3bb33aa88f96388343ee19e
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-06-07 10:19:04 +0200

    Update Stratagems.vue

--- components/detachments/desktop/Stratagems.vue
+++ components/detachments/desktop/Stratagems.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-3 px-2 md:px-15">
-    <h3 class="text-2xl font-bebas mt-5">Stratagems</h3>
+    <h3 class="text-2xl font-bebas mt-5 px-13 ">Stratagems</h3>
 
     <div class="grid grid-cols-1 md:grid-cols-2 md:gap-4">
       <div v-for="stratagem in stratagems">

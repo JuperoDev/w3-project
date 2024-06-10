df196a74a5ce2d1039ed2a537d96953b78311433
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-06-10 17:53:00 +0200

    kinda works sandbox

--- components/armyBuilder/armyComposer.vue
+++ components/armyBuilder/armyComposer.vue
@@ -1,17 +1,50 @@
 <template>
     <div class="armyComposer_container">
-        <div class="armyComposer_container_character bg-zinc-800 text-zinc-50 uppercase">
-            
-            
-            character 160 points<v-btn>+</v-btn> </div>
+      <div class="armyComposer_container_character bg-zinc-800 text-zinc-50 uppercase">
+        character 160 points
+        <v-btn @click="openDialog">+</v-btn>
+      </div>
+  
+      {{ url }}
+      <p>Unit 1</p>
+      <p>70</p>
+  
+      <v-dialog v-model="isDialogOpen" max-width="500">
+        <template v-slot:default="{ isActive }">
+          <v-card title="Dialog">
+            <v-card-text>
+              <div v-for="character in characters" :key="character.unitName">
+                {{ character.unitName }}: {{ character.basicPoints }} points
+              </div>
+            </v-card-text>
+  
+            <v-card-actions>
+              <v-spacer></v-spacer>
+  
+              <v-btn text="Close Dialog" @click="isDialogOpen = false"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </div>
-{{ url }}
-    <p>Unit 1</p> <p>70</p>
-</template>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  // props
+  const props = defineProps({
+    url: String
+  });
+  
+  const isDialogOpen = ref(false);
+  const characters = ref([]);
+  
+  const openDialog = () => {
+    isDialogOpen.value = true;
+  };
+  
+  // Import JSON data when the component is mounted
 
-<script setup>
-//props
-const props = defineProps({
-  url: String
-});
-</script>
+  </script>
+  

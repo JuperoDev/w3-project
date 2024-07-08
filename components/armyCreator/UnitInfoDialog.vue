025c5353c54dbbaa4d0c7007ecab28e0c8b6be7c
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-07-08 21:43:42 +0200

    init unit json display

---
+++ components/armyCreator/UnitInfoDialog.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <v-btn icon small @click="dialog = true">
+        <v-icon small>mdi-information</v-icon>
+      </v-btn>
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <v-card-title class="headline">Unit Info</v-card-title>
+          <v-card-text>
+            <p>{{ url }} {{ unitName }}</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps({
+    url: {
+      type: String,
+      required: true
+    },
+
+    unitName: {
+        type: String,
+        required: true
+    }
+  });
+  
+  const dialog = ref(false);
+  </script>
+  

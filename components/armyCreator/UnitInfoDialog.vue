4a070c89a601f7b414eaecc51b4374ae2effe055
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-08-10 00:45:58 +0200

    updated dialog info for units

--- components/armyCreator/UnitInfoDialog.vue
+++ components/armyCreator/UnitInfoDialog.vue
@@ -8,7 +8,8 @@
     </span>
     <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
       <v-card>
-        <v-toolbar dark color="primary">
+        <!-- <v-toolbar dark color="primary"> -->
+          <v-toolbar dark >
           <v-btn icon dark @click="dialog = false">
             <v-icon>mdi-close</v-icon>
           </v-btn>
@@ -66,6 +67,70 @@
                 </v-expansion-panel-title>
                 <v-expansion-panel-text>
                   <MobileWarGearAbilities :warGearAbilities="jsonData.warGearAbilities" />
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Additional Parameter -->
+              <v-expansion-panel v-if="jsonData.additionalParameter && Object.keys(jsonData.additionalParameter).length > 0">
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">{{ jsonData.additionalParameter.title }}</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <MobileAdditionalParameter :additionalParameter="jsonData.additionalParameter" />
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Supreme Commander -->
+              <v-expansion-panel v-if="jsonData.supremeCommander">
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">Supreme Commander</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <p>If this model is in your army, it must be your Warlord</p>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Damaged -->
+              <v-expansion-panel v-if="jsonData.damaged && Object.keys(jsonData.damaged).length > 0">
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">Damaged: 1 - {{ jsonData.damaged.remainingWounds }} wounds remaining</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <p>{{ jsonData.damaged.description }}</p>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Wargear Options -->
+              <v-expansion-panel>
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">Wargear Options</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <MobileWargear :wargear="jsonData.wargear" />
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Leader -->
+              <v-expansion-panel v-if="jsonData.leader && jsonData.leader.length">
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">Leader</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <MobileLeader :leader="jsonData.leader" :leaderInfo="jsonData.leaderInfo" />
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+
+              <!-- Unit Composition -->
+              <v-expansion-panel>
+                <v-expansion-panel-title>
+                  <div class="uppercase font-semibold">Unit Composition</div>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <MobileUnitComposition
+                    :options="jsonData.options"
+                    :unitComposition="jsonData.unitComposition"
+                    :equipment="jsonData.equipment"
+                  />
                 </v-expansion-panel-text>
               </v-expansion-panel>
 

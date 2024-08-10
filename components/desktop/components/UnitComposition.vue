e9e763627a55e71f318141199b6085a759e7406a
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-08-10 18:25:40 +0200

    updates

--- components/desktop/components/UnitComposition.vue
+++ components/desktop/components/UnitComposition.vue
@@ -1,58 +1,49 @@
 <template>
+  <div class="datasheet-desktop__section-header m-3">
+    <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
+      <p>Unit Composition</p>
+    </div>
+<div class="unit-composition__wrap mb-3">
     <div
-          class="datasheet-desktop__section-header  m-3"
-        >
-        <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
-          <p>Unit Composition</p>
-        </div>
-
-        <div v-for="unit in unitComposition" :key="unit.unitType" 
-        
-        >
-          <div class="unit-composition__container flex m-2">
-            <p>
-        <template v-if="unit.minQuantity == unit.maxQuantity">
-          {{ unit.minQuantity }}</template
-        >
-        <template v-if="unit.minQuantity !== unit.maxQuantity"
-          >{{ unit.minQuantity }} - {{ unit.maxQuantity }}</template
-        >
-
-
-      </p>
-
-      <p class="capitalize px-2">
-<div class="unit-composition__equiment-grid">
-  {{ unit.unitType }}
-</div>
-   
-</p>
-
-          </div>
+      v-for="(unit, index) in unitComposition"
+      :key="unit.unitType"
+      :class="{
+        'bg-zinc-200 text-gray-900 py-1  border-b-2 border-solid border-zinc-400': (index + 1) % 2 === 0,
+        'bg-white text-gray-900 py-1  border-b-2 border-solid border-zinc-400': (index + 1) % 2 !== 0,
+      }"
      
+    >
+      <div class="unit-composition__container flex m-2">
+        <p>
+          <template v-if="unit.minQuantity == unit.maxQuantity">
+            {{ unit.minQuantity }}
+          </template>
+          <template v-if="unit.minQuantity !== unit.maxQuantity">
+            {{ unit.minQuantity }} - {{ unit.maxQuantity }}
+          </template>
+        </p>
 
-        </div>
-        
-        <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
-          <p>Equipment</p>
-        </div>
-        <p v-for="(unit, index) in unitComposition" :key="index" class="capitalize">
-    <b>{{ unit.unitType }}:</b> {{ unit.equipment.join(', ') }}
+        <p class="capitalize px-2">
+          <div class="unit-composition__equiment-grid">
+            {{ unit.unitType }}
+          </div>
+        </p>
+      </div>
    
-  </p>
+   
+    </div>
+  </div>
+    <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
+      <p>Equipment</p>
+    </div>
+    <p v-for="(unit, index) in unitComposition" :key="index"   :class="{
+        'bg-zinc-200 capitalize text-gray-900 py-2 px-2  border-b-2 border-solid border-zinc-400': (index + 1) % 2 === 0,
+        'bg-white capitalize text-gray-900 py-2 px-2 border-b-2 border-solid border-zinc-400': (index + 1) % 2 !== 0,
+      }">
+      <b>{{ unit.unitType }}:</b> {{ unit.equipment.join(', ') }}
+    </p>
 
- 
-    <!-- <p class="py-2">Every model is equipped with:</p>
-    <div v-for="equip in equipment" class="px-5 capitalize">
-      <ul>
-        <li>
-          
-          {{ equip }}
-        </li>
-      </ul>
-    </div> -->
     <br />
-
     <br />
     <div class="grid grid-cols-4 bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
       <p class="col-span-2 m-auto">Model name</p>
@@ -62,10 +53,10 @@
 
     <div class="grid grid-row grid-cols-4" v-for="option in options">
       <div class="col-span-2">
-        <div class=" items-center capitalize p-3">
+        <div class="items-center capitalize p-3">
           <div v-for="unit in unitComposition">
-            <p>{{ unit.unitType }} </p>
-            <br/>
+            <p>{{ unit.unitType }}</p>
+            <br />
           </div>
         </div>
       </div>
--- components/mobile/UnitComposition.vue
+++ components/mobile/UnitComposition.vue
@@ -1,29 +1,51 @@
 <template>
   <div>
-    <div v-for="unit in unitComposition" :key="unit.unitType" class="flex m-2">
-          
-          <p>
-            <template v-if="unit.minQuantity == unit.maxQuantity">
-              {{ unit.minQuantity }}</template
-            >
-            <template v-if="unit.minQuantity !== unit.maxQuantity"
-              >{{ unit.minQuantity }} - {{ unit.maxQuantity }}</template
-            >
-    
-    
-          </p>
-    
-          <p class="capitalize px-2">
-    
-       {{ unit.unitType }}
-    </p>
-        </div>
+    <div class="unit-composition__wrap mb-3">
+    <div
+      v-for="(unit, index) in unitComposition"
+      :key="unit.unitType"
+      :class="{
+        'bg-zinc-200 text-gray-900 py-1  border-b-2 border-solid border-zinc-400': (index + 1) % 2 === 0,
+        'bg-white text-gray-900 py-1  border-b-2 border-solid border-zinc-400': (index + 1) % 2 !== 0,
+      }"
+     
+    >
+      <div class="unit-composition__container flex m-2">
+        <p>
+          <template v-if="unit.minQuantity == unit.maxQuantity">
+            {{ unit.minQuantity }}
+          </template>
+          <template v-if="unit.minQuantity !== unit.maxQuantity">
+            {{ unit.minQuantity }} - {{ unit.maxQuantity }}
+          </template>
+        </p>
+
+        <p class="capitalize px-2">
+          <div class="unit-composition__equiment-grid">
+            {{ unit.unitType }}
+          </div>
+        </p>
+      </div>
+   
+   
+    </div>
+  </div>
 
     <br />
     <div class="datasheet-desktop__section-header bg-zinc-900 text-zinc-100 rounded-t-lg p-3">
           <p>Equipment</p>
         </div>
-        <p v-for="(unit, index) in unitComposition" :key="index" class="capitalize">
+        <p v-for="(unit, index) in unitComposition" :key="index" 
+        
+        :class="{
+        'bg-zinc-200 capitalize text-gray-900 py-2 px-2  border-b-2 border-solid border-zinc-400': (index + 1) % 2 === 0,
+        'bg-white capitalize text-gray-900 py-2 px-2 border-b-2 border-solid border-zinc-400': (index + 1) % 2 !== 0,
+      }"
+        
+        
+        >
+
+
     <b>{{ unit.unitType }}:</b> {{ unit.equipment.join(', ') }}
    
   </p>

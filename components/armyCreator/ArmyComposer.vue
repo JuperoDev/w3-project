af34a1e09b785c73281bd85de919b4b69e0c53e7
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-08-15 12:46:41 +0200

    feature: allied units

--- components/armyCreator/ArmyComposer.vue
+++ components/armyCreator/ArmyComposer.vue
@@ -8,15 +8,15 @@
         <p class="capitalize"><strong >Detachment:</strong> {{ selectedDetachment }}</p>
         <p><strong>Total Points:</strong> {{ totalPoints }}</p>
         <ArmyExporter 
-        :armyIndex="armyIndex" 
-        :totalPoints="totalPoints"
-        :characterUnits="characterUnits"
-        :battlelineUnits="battlelineUnits"
-        :otherUnits="otherUnits"
-        :dedicatedTransportUnits="dedicatedTransportUnits"
-      />
+          :armyIndex="armyIndex" 
+          :totalPoints="totalPoints"
+          :characterUnits="characterUnits"
+          :battlelineUnits="battlelineUnits"
+          :otherUnits="otherUnits"
+          :dedicatedTransportUnits="dedicatedTransportUnits"
+          :alliedUnits="alliedUnits"
+        />
       </div>
-      
     </div>
 
     <Characters 
@@ -45,7 +45,6 @@
       @update-total-points="updateDedicatedTransportPoints"
       @update-units="updateDedicatedTransportUnits"
     />
-    
     <Other 
       :url="sanitizedUrl" 
       :selectedDetachment="selectedDetachment"
@@ -54,9 +53,14 @@
       @update-total-points="updateOtherPoints"
       @update-units="updateOtherUnits"
     />
-   
-
-    <!-- PointStatus component -->
+    <AlliedUnits
+      :url="sanitizedUrl"
+      :armyIndex="armyIndex"
+      :selectedDetachment="selectedDetachment"
+      class="mt-4 mb-24"
+      @update-total-points="updateAlliedPoints"
+      @update-units="updateAlliedUnits"
+    />
     <PointStatus :totalPoints="totalPoints" :pointList="pointList" />
   </div>
 </template>
@@ -66,53 +70,35 @@ import { ref, computed } from 'vue';
 import Characters from './Characters.vue';
 import Battleline from './Battleline.vue';
 import Other from './Other.vue';
-import DedicatedTransports from './DedicatedTransports.vue';  // Import the new component
+import DedicatedTransports from './DedicatedTransports.vue';
+import AlliedUnits from './AlliedUnits.vue';
 import ArmyExporter from './ArmyExporter.vue';
-import PointStatus from './PointStatus.vue';  // Import the new component
+import PointStatus from './PointStatus.vue';
 
 const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  },
-  selectedFaction: {
-    type: String,
-    required: true
-  },
-  selectedArmy: {
-    type: String,
-    required: true
-  },
-  pointList: {
-    type: String,
-    required: true
-  },
-  selectedDetachment: {
-    type: String,
-    required: true
-  },
-  url: {
-    type: String,
-    required: true
-  },
-  armyIndex: {
-    type: Number,
-    required: true
-  }
+  name: String,
+  selectedFaction: String,
+  selectedArmy: String,
+  pointList: String,
+  selectedDetachment: String,
+  url: String,
+  armyIndex: Number
 });
 
 const characterPoints = ref(0);
 const battlelinePoints = ref(0);
 const otherPoints = ref(0);
-const dedicatedTransportPoints = ref(0); // Add this
+const dedicatedTransportPoints = ref(0);
+const alliedPoints = ref(0);
 
 const characterUnits = ref([]);
 const battlelineUnits = ref([]);
 const otherUnits = ref([]);
-const dedicatedTransportUnits = ref([]); // Add this
+const dedicatedTransportUnits = ref([]);
+const alliedUnits = ref([]);
 
 const totalPoints = computed(() => {
-  return characterPoints.value + battlelinePoints.value + otherPoints.value + dedicatedTransportPoints.value; // Include this
+  return characterPoints.value + battlelinePoints.value + otherPoints.value + dedicatedTransportPoints.value + alliedPoints.value;
 });
 
 const updateCharacterPoints = (points) => {
@@ -127,8 +113,12 @@ const updateOtherPoints = (points) => {
   otherPoints.value = points;
 };
 
-const updateDedicatedTransportPoints = (points) => { // Add this
+const updateDedicatedTransportPoints = (points) => {
   dedicatedTransportPoints.value = points;
+};
+
+const updateAlliedPoints = (points) => {
+  alliedPoints.value = points;
 };
 
 const updateCharacterUnits = (units) => {
@@ -143,8 +133,12 @@ const updateOtherUnits = (units) => {
   otherUnits.value = units;
 };
 
-const updateDedicatedTransportUnits = (units) => { // Add this
+const updateDedicatedTransportUnits = (units) => {
   dedicatedTransportUnits.value = units;
+};
+
+const updateAlliedUnits = (units) => {
+  alliedUnits.value = units;
 };
 
 const sanitizedUrl = computed(() => {

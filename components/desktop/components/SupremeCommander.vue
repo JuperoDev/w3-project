aa5f75ff08c0d8cd06a940e1294c62f84d2378bf
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-08-31 01:54:40 +0200

    update dark angels

--- components/desktop/components/SupremeCommander.vue
+++ components/desktop/components/SupremeCommander.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="datasheet-desktop__leader-container">
       <div class="datasheet-desktop__leader--header bg-zinc-900 text-zinc-100 rounded-t-lg p-3 mx-3">
-        <p>Supremme Commander</p>
+        <p>Supreme Commander</p>
       </div>
   
   <div class="datasheet-desktop__leader--body my-3 pl-5 ">

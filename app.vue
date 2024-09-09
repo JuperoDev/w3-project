0dbd57cc762e5f4180466ad5a118340163ae81d7
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-09-09 15:01:08 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-      <!-- <GeneralPurposePasswordProtection>   -->
+      <GeneralPurposePasswordProtection>   
       
       <NuxtPage />
       
-    <!-- </GeneralPurposePasswordProtection>    -->
+     </GeneralPurposePasswordProtection>    
     <GeneralPurposeScrollToTopArrow />
   </div>
 </template>

3c18977eb3b996ca5b58c0c6f0b9b03faa04315b
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-10-16 00:10:06 +0200

    Update index.vue

--- pages/[faction]/index.vue
+++ pages/[faction]/index.vue
@@ -2,7 +2,7 @@
   <div>
     
     <NavbarComponentsButtonGrid />
-    <div class="bg-orange-100 p-2 md:px-10 lg:px-16 h-full relative pb-20">  
+    <div class="bg-orange-500 p-2 md:px-10 lg:px-16 min-h-screen relative pb-20">  
 
 <!-- <GeneralPurposeFireSparks /> -->
 

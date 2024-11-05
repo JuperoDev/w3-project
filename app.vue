e0e44f54b0bd527ea2586a77756e0c9a6a4dad9f
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-11-05 17:31:40 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="app-container">
     <GeneralPurposePasswordProtection>
-      <NuxtLayout>
+      
         <NuxtPage />
-      </NuxtLayout>
+    
     </GeneralPurposePasswordProtection>
     <GeneralPurposeScrollToTopArrow />
   </div>
 </template>
-
-<script setup>
-// No additional script setup needed
-</script>
-
-<style>
-:root {
-  overscroll-behavior: none;
-}
-</style>

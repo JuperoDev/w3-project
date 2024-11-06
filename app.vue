c35b737296369247fb95f942e706b9638f399a68
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2024-11-06 18:35:27 +0100

    fixed miniature mixing

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-container">
     <GeneralPurposePasswordProtection>
-      
+    <NuxtLayout>
         <NuxtPage />
-    
+      </NuxtLayout>
     </GeneralPurposePasswordProtection>
     <GeneralPurposeScrollToTopArrow />
   </div>
